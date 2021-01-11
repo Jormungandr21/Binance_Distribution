c856f5e303e6e55649f66a8b3e5e0c0799f6c806
Fareen Pourmoussavian <fpourmoussavian@gmail.com>
2021-01-11 18:27:51 -0500

    Add files via upload

--- Binance.py
+++ Binance.py
@@ -47,8 +47,12 @@ def Binance_Wallet():#Shows Nonzero wallet balance on Binance
 				asset['Total $']=str(val) #get total $ amount for that coin
 				pair_btc_total=(float(asset.get('free'))+float(asset.get('locked')))/BTC_price
 				asset.update({'BTC Total':round(pair_btc_total,8)}) #round btc amount to 8 decimals
-				asset.update({'Market Price':round(BTC_price,8)}) #round market price amount to 8 decimals
+				asset.update({'Market Price':round(BTC_price,3)}) #round market price amount to 8 decimals
 				continue
+			
+			if(ticker=="VTHOBTC"):
+				continue
+					
 			pair_ticker=client.get_avg_price(symbol=ticker)
 			pair_price=float(pair_ticker.get('price'))
 			val=round(((float(asset.get('free'))+float(asset.get('locked')))*pair_price*BTC_price),2) #calculating $ amount of coin
@@ -66,22 +70,17 @@ def Binance_Wallet():#Shows Nonzero wallet balance on Binance
 		asset['available']=asset.pop('free') #changing key 'free' to 'available' cause more intuitive
 		
 	def WalletDistribution():
-		user_input=input("Do you want to see your wallet distribution?(Y/N) ") 
-		if(user_input=='Y' or user_input=='y' or user_input=='YES' or user_input=='Yes' or user_input=='yes'):	
-			dataframe=panda.DataFrame(newList) #turn data into dataframe to use in creating table and pie chart
-			df=dataframe.sort_values(by=['BTC Total'],ascending=False) #sort by btc total descending so highest first sorted to last
-			wallet_distribution=make_subplots(rows=1, cols=2, specs=[[{"type":"table"},{"type":"pie"}]])#imagine a table with 2 columns. One is for table and One for pie chart
-			wallet_distribution.add_trace(graph.Pie(name="Total $=",values=df['Total $'],title="Wallet Distribution 			         \nFor Legend on right, click on a coin to hide it from pie chart.\nDouble click a coin in the legend on the right to focus it",labels=df['asset'],showlegend=True),row=1,col=2)
-			#pie chart, show labels and show legend so when you hover over, it gives $ amount invested into that coin
-			wallet_distribution.add_trace(graph.Table(header=dict(values=["Coin","Available","Locked","BTC Total","Total $","Market Price(BTC pair)","TOTAL BTC","TOTAL $"],align="center"),
-													   cells=dict(values=[df['asset'],df['available'],df['locked'],df['BTC Total'],'$'+df['Total $'],df['Market Price'],btc_total_amount,'$'+str(btc_usd_amount)],align="center")),row=1,col=1)
-			#table shows all colum names and values associated with those columns
-			wallet_distribution.show()
+		
+		dataframe=panda.DataFrame(newList) #turn data into dataframe to use in creating table and pie chart
+		df=dataframe.sort_values(by=['BTC Total'],ascending=False) #sort by btc total descending so highest first sorted to last
+		wallet_distribution=make_subplots(rows=1, cols=2, specs=[[{"type":"table"},{"type":"pie"}]])#imagine a table with 2 columns. One is for table and One for pie chart
+		wallet_distribution.add_trace(graph.Pie(name="Total $=",values=df['Total $'],title="Wallet Distribution 			         \nFor Legend on right, click on a coin to hide it from pie chart.\nDouble click a coin in the legend on the right to focus it",labels=df['asset'],showlegend=True),row=1,col=2)
+		#pie chart, show labels and show legend so when you hover over, it gives $ amount invested into that coin
+		wallet_distribution.add_trace(graph.Table(header=dict(values=["Coin","Available","Locked","BTC Total","Total $","Market Price(BTC pair)","TOTAL BTC","TOTAL $"],align="center"),
+												   cells=dict(values=[df['asset'],df['available'],df['locked'],df['BTC Total'],'$'+df['Total $'],df['Market Price'],btc_total_amount,'$'+str(btc_usd_amount)],align="center")),row=1,col=1)
+		#table shows all colum names and values associated with those columns
+		wallet_distribution.show()
 					
-	
-		else: #if user selects anything other than Y, y, Yes, yes, YES automativally exit
-			return 
-
 	def Percentage(l, datavalues): #calculates % for pie chart
 	    values = int(l / 100.*np.sum(datavalues)) 
 	    return "{:.1f}%\n(${:d})".format(l, values) 	
